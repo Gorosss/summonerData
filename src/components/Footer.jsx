303c451b91e3eb2cc515609cc6ea8ffbbb517b60
Gorosss <90179487+Gorosss@users.noreply.github.com>
2024-01-31 21:21:41 +0100

    Footer fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
+import { Github, LinkedIn } from '../assets/svgs.jsx'
+
+
 import '../css/Footer.css'
 
 
-export function Footer (){
+export function Footer() {
 
-  return (
-    <footer >
-      <p>&copy; {new Date().getFullYear()} SummonerData.gg. Todos los derechos reservados.</p>
-      {/* Agrega más información o enlaces según sea necesario */}
-    </footer>
-  );
+    return (
+        <footer >
+            <p>&copy; {new Date().getFullYear()} SummonerData.gg. All rights reserved.</p>
+            <a
+                style={{ marginRight: '5px', marginLeft: '20px', textDecoration: 'none', color: 'inherit'}}
+                href="https://github.com/Gorosss"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Github /> Github
+            </a>
+
+
+            <a
+                style={{ marginRight: '5px', marginLeft: '5px', textDecoration: 'none', color: 'inherit' }}
+                href="https://www.linkedin.com/in/jon-gorostegui-martinez-a0571a247/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <LinkedIn /> LinkedIn
+            </a>
+
+        </footer>
+    );
 };
 
 export default Footer;

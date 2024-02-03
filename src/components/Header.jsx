f6e6ee9df42c1954af458780b4474aa369337685
Gorosss <90179487+Gorosss@users.noreply.github.com>
2024-02-03 13:18:54 +0100

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,53 @@
 import '../css/Header.css'
-import React from 'react';
+import React, { useState  } from 'react';
+
+import { useNavigate  } from 'react-router-dom';
+import {RegionSelect} from '../components/RegionSelect'
+
+
 
 
 export function Header() {
+
+    const [formData, setFormData] = useState({
+        summonerName: '',
+        region: 'euw', // Set a default region if needed
+      });
+    
+      const navigate = useNavigate ();
+
+
+
+      const handleFormSubmit = (event) => {
+        event.preventDefault()
+    
+
+        
+
+        const { summonerName, region } = formData;
+
+
+        console.log(summonerName, region);
+
+
+
+    
+        const url = `/profile/${region}/${summonerName}`;
+
+        
+        navigate(url);
+      };
+
+
+      const handleInputChange = (event) => {
+        const { name, value } = event.target;
+    
+        // Update the form data state when form fields change
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
 
 
     const handleHome = () => {
@@ -18,9 +63,20 @@ export function Header() {
                     SummonerData.gg
                 </div>
                 <div className='searchSummoner'>
-                    <form className='form' onSubmit={null}>
-                        <input placeholder='Find Summoner' />
-                        <button type='submit'> Search </button>
+                    <form className='form'  onSubmit={handleFormSubmit}>
+                    <input
+                        type='text'
+                        name='summonerName'
+                        placeholder='Summoner name ...'
+                        value={formData.summonerName}
+                        onChange={handleInputChange}
+                    />
+                    <RegionSelect
+                        name='region'
+                        value={formData.region}
+                        onChange={handleInputChange}
+                    />
+                    <button type='submit'> Search </button>
                     </form>
                 </div>
             </div>
